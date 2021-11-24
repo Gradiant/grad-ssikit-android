2fd4f0e57e16f8bbb26fc606acea1a024735c562
Xavier Martinez <xmartinez@gradiant.org>
2021-11-24 18:34:07 +0100

    Updated

--- src/main/kotlin/id/walt/custodian/Custodian.kt
+++ src/main/kotlin/id/walt/custodian/Custodian.kt
@@ -19,17 +19,17 @@ import id.walt.vclib.model.VerifiableCredential
 abstract class Custodian : WaltIdService() {
     override val implementation get() = serviceImplementation<Custodian>()
 
-    override fun generateKey(keyAlgorithm: KeyAlgorithm): Key = ContextManager.keyStore.load(keyService.generate(keyAlgorithm).id)
-    override fun getKey(alias: String): Key = ContextManager.keyStore.load(alias)
-    override fun listKeys(): List<Key> = ContextManager.keyStore.listKeys()
-    override fun storeKey(key: Key) = ContextManager.keyStore.store(key)
-    override fun deleteKey(id: String) = ContextManager.keyStore.delete(id)
+    open fun generateKey(keyAlgorithm: KeyAlgorithm): Key = implementation.generateKey(keyAlgorithm)
+    open fun getKey(alias: String): Key = implementation.getKey(alias)
+    open fun listKeys(): List<Key> = implementation.listKeys()
+    open fun storeKey(key: Key): Unit = implementation.storeKey(key)
+    open fun deleteKey(id: String): Unit = implementation.deleteKey(id)
 
-    override fun getCredential(id: String) = ContextManager.vcStore.getCredential(id, VC_GROUP)
-    override fun listCredentials(): List<VerifiableCredential> = ContextManager.vcStore.listCredentials(VC_GROUP)
-    override fun listCredentialIds(): List<String> = ContextManager.vcStore.listCredentialIds(VC_GROUP)
-    override fun storeCredential(alias: String, vc: VerifiableCredential) = ContextManager.vcStore.storeCredential(alias, vc, VC_GROUP)
-    override fun deleteCredential(alias: String) = ContextManager.vcStore.deleteCredential(alias, VC_GROUP)
+    open fun getCredential(id: String): VerifiableCredential? = implementation.getCredential(id)
+    open fun listCredentials(): List<VerifiableCredential> = implementation.listCredentials()
+    open fun listCredentialIds(): List<String> = implementation.listCredentialIds()
+    open fun storeCredential(alias: String, vc: VerifiableCredential): Unit = implementation.storeCredential(alias, vc)
+    open fun deleteCredential(alias: String): Boolean = implementation.deleteCredential(alias)
 
     open fun createPresentation(
         vcs: List<String>, holderDid: String, verifierDid: String? = null, domain: String? = null, challenge: String? = null
@@ -46,17 +46,17 @@ open class WaltIdCustodian : Custodian() {
     private val jwtCredentialService = JwtCredentialService.getService()
     private val jsonLdCredentialService = JsonLdCredentialService.getService()
 
-    override fun generateKey(keyAlgorithm: KeyAlgorithm): Key = WaltContext.keyStore.load(keyService.generate(keyAlgorithm).id)
-    override fun getKey(alias: String): Key = WaltContext.keyStore.load(alias)
-    override fun listKeys(): List<Key> = WaltContext.keyStore.listKeys()
-    override fun storeKey(key: Key) = WaltContext.keyStore.store(key)
-    override fun deleteKey(id: String) = WaltContext.keyStore.delete(id)
+    override fun generateKey(keyAlgorithm: KeyAlgorithm): Key = ContextManager.keyStore.load(keyService.generate(keyAlgorithm).id)
+    override fun getKey(alias: String): Key = ContextManager.keyStore.load(alias)
+    override fun listKeys(): List<Key> = ContextManager.keyStore.listKeys()
+    override fun storeKey(key: Key) = ContextManager.keyStore.store(key)
+    override fun deleteKey(id: String) = ContextManager.keyStore.delete(id)
 
-    override fun getCredential(id: String) = WaltContext.vcStore.getCredential(id, VC_GROUP)
-    override fun listCredentials(): List<VerifiableCredential> = WaltContext.vcStore.listCredentials(VC_GROUP)
-    override fun listCredentialIds(): List<String> = WaltContext.vcStore.listCredentialIds(VC_GROUP)
-    override fun storeCredential(alias: String, vc: VerifiableCredential) = WaltContext.vcStore.storeCredential(alias, vc, VC_GROUP)
-    override fun deleteCredential(alias: String) = WaltContext.vcStore.deleteCredential(alias, VC_GROUP)
+    override fun getCredential(id: String) = ContextManager.vcStore.getCredential(id, VC_GROUP)
+    override fun listCredentials(): List<VerifiableCredential> = ContextManager.vcStore.listCredentials(VC_GROUP)
+    override fun listCredentialIds(): List<String> = ContextManager.vcStore.listCredentialIds(VC_GROUP)
+    override fun storeCredential(alias: String, vc: VerifiableCredential) = ContextManager.vcStore.storeCredential(alias, vc, VC_GROUP)
+    override fun deleteCredential(alias: String) = ContextManager.vcStore.deleteCredential(alias, VC_GROUP)
 
     override fun createPresentation(
         vcs: List<String>, holderDid: String, verifierDid: String?, domain: String?, challenge: String?
@@ -66,4 +66,3 @@ open class WaltIdCustodian : Custodian() {
         else -> throw IllegalStateException("All verifiable credentials must be of the same proof type.")
     }
 }
-
